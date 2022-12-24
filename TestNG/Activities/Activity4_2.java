77a29b71ff569ce509738ac544b2aeb4d3c15fa7
srenaray <103926005+srenaray@users.noreply.github.com>
2022-12-24 15:04:40 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4_2.java
@@ -0,0 +1,12 @@
+package Testng.Activities;
+
+import org.testng.annotations.Test;
+
+public class Activity4_2 {
+
+	 @Test
+	    public void TestCase() {
+	        System.out.println("I'm in the test case from DemoTwo Class");
+	
+}
+}
